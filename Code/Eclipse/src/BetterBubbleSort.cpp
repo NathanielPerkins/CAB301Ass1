8a8c8fca3450530fcb3cb1a1d0d467c023b8fdf2
NathanielPerkins <thallazar@gmail.com>
2016-03-22 13:18:12 +1000

    Working Bubble Sort

--- Code/Eclipse/src/BetterBubbleSort.cpp
+++ Code/Eclipse/src/BetterBubbleSort.cpp
@@ -9,7 +9,34 @@
 #include <iostream>
 using namespace std;
 
+bool BetterBubbleSort(int array[], int size);
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int k = 0;
+	int array[] = {6,2,3,4,6,5};
+	int size = 6;
+	BetterBubbleSort(array,size);
+	for(int i = 0; i<size;i++){
+		cout<< array[i];
+	}
 	return 0;
+}
+
+bool BetterBubbleSort(int array[], int size){
+	int count = size;
+	bool sflag = true;
+	// insert error checking here
+
+	while (sflag){
+		sflag = false;
+		for (int j = 0; j<count-1;j++){
+			if (array[j+1] < array[j]){
+				int temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+				sflag = true;
+			}
+		}
+		count--;
+	}
+	return true;
 }

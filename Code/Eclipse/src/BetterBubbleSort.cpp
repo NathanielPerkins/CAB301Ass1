d03a45ee64b4caa3450b2a260f087d1a6c04f6ac
NathanielPerkins <thallazar@gmail.com>
2016-03-22 13:06:46 +1000

    Initialize Eclipse Project

---
+++ Code/Eclipse/src/BetterBubbleSort.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BetterBubbleSort.cpp
+// Author      : Nathan Perkins
+// Version     :
+// Copyright   : No Copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}

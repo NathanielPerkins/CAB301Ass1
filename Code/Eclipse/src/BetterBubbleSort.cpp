823745048aa04e41125adf2a3219d4a12ce4eadf
NathanielPerkins <thallazar@gmail.com>
2016-03-28 14:49:17 +1000

    Added random array creation and checking

--- Code/Eclipse/src/BetterBubbleSort.cpp
+++ Code/Eclipse/src/BetterBubbleSort.cpp
@@ -7,25 +7,39 @@
 //============================================================================
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 bool BetterBubbleSort(int array[], int size);
+bool IsSorted(int array[],int size);
+void SortedPrint(int array[], int size);
+void PrintArray(int array[], int size);
+void GenerateArray(int array[], int size);
+void GenerateOrderedArray(int array[],int size);
 int main() {
-	int k = 0;
-	int array[] = {6,2,3,4,6,5};
-	int size = 6;
-	BetterBubbleSort(array,size);
-	for(int i = 0; i<size;i++){
-		cout<< array[i];
-	}
+	int size = 100;
+	int array[size];
+	GenerateOrderedArray(array,size);
+	PrintArray(array,size);
+	clock_t start;
+	double duration;
+    start = clock();
+
+    BetterBubbleSort(array,size);
+
+	duration = ( clock() - start ) / (double) CLOCKS_PER_SEC;
+	cout<<endl<<"Time Taken: "<< duration <<'\n';
+
+
+	PrintArray(array,size);
+	SortedPrint(array, size);
 	return 0;
 }
 
 bool BetterBubbleSort(int array[], int size){
 	int count = size;
 	bool sflag = true;
-	// insert error checking here
-
 	while (sflag){
 		sflag = false;
 		for (int j = 0; j<count-1;j++){
@@ -39,4 +53,42 @@ bool BetterBubbleSort(int array[], int size){
 		count--;
 	}
 	return true;
+}
+
+bool IsSorted(int array[], int size){
+	for(int i=1;i<size;i++){
+		if(array[i]<array[i-1]){
+			return false;
+		}
+	}
+	return true;
+}
+void SortedPrint(int array[], int size){
+	cout<<endl;
+	if (IsSorted(array,size)){
+		cout<<"This array is sorted"<<endl;
+	}
+	else{
+		cout<<"This array is not sorted"<<endl;
+	}
+}
+void PrintArray(int array[],int size){
+	for(int i = 0; i<size;i++){
+		cout<< array[i]<< " ";
+	}
+}
+void GenerateArray(int array[], int size){
+	srand(time(NULL));
+	for(int i = 0; i<size;i++){
+		int temp = rand()%100+1;
+		array[i] = temp;
+	}
+}
+void GenerateOrderedArray(int array[], int size){
+	srand(time(NULL));
+	array[0] = 1;
+	for(int i = 1; i<size;i++){
+		int temp = rand()%10+array[i-1];
+		array[i] = temp;
+	}
 }
